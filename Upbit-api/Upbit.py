24b75afaeb9dcdd76faaf97b51aa7f54689ff11f
kfru5515 <kfru5515@naver.com>
2023-12-18 11:08:27 -0800

    balance
    -

--- Upbit-api/Upbit.py
+++ Upbit-api/Upbit.py
@@ -1,24 +1,46 @@
 import ccxt
 import config
-import os
-import sys
 
 upbit = ccxt.upbit({
     'enbaleRateLimit' : True,
     'apiKey': config.Ubpit_ApiKey,
     'secret': config.Ubpit_SecretKey,
 })
-
 balance = upbit.fetch_balance()
+krw_balance = balance['total']['KRW']
+
+total_krw_value = 0
+
+symbols = upbit.load_markets()
+chosen_symbol = 'BTC/KRW' 
+
+for symbol in symbols:
+    print(symbol)
+
+print("Available Trading Pairs on Upbit:")
+
+
 
 print("Account Balance:")
+print(f"Current KRW Balance: {krw_balance} KRW")
+
+
+#total balance
 for currency, amount in balance['total'].items():
-    print(f"{currency}: {amount}")
-'''
-markets = upbit.load_markets('KRW')
-closed_orders = upbit.fetch_closed_orders('SHIB/KRW')
-balance = upbit.fetch_balance()
+    if currency != 'KRW' and amount > 0:
+        try:
+            market_price = upbit.fetch_ticker(f'{currency}/KRW')['close']
 
+            currency_krw_value = amount * market_price
 
-print(closed_orders)
-'''
+            total_krw_value += currency_krw_value
+
+            print(f"{currency}: {amount} {currency} (Market Price: {market_price} KRW) - Value: {currency_krw_value:.2f} KRW")
+        except ccxt.NetworkError as e:
+            print(f"Network error while fetching data for {currency}/{chosen_symbol}: {e}")
+        except ccxt.ExchangeError as e:
+            print(f"Exchange error while fetching data for {currency}/{chosen_symbol}: {e}")
+        except Exception as e:
+            print(f"An error occurred for {currency}/{chosen_symbol}: {e}")
+    print(f"\nTotal Value of All Currencies in KRW: {total_krw_value:.2f} KRW")
+
