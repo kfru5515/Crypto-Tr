f0e1dd980f53b4faa877edb05897c752600698e9
kfru5515 <kfru5515@naver.com>
2023-12-17 17:42:49 -0800

    Update Upbit.py

--- Upbit-api/Upbit.py
+++ Upbit-api/Upbit.py
@@ -1,6 +1,24 @@
 import ccxt
+import config
+import os
+import sys
 
-Upbit = ccxt.Upbit()
-markets = Upbit.load_market()
+upbit = ccxt.upbit({
+    'enbaleRateLimit' : True,
+    'apiKey': config.Ubpit_ApiKey,
+    'secret': config.Ubpit_SecretKey,
+})
 
-print(markets)
+balance = upbit.fetch_balance()
+
+print("Account Balance:")
+for currency, amount in balance['total'].items():
+    print(f"{currency}: {amount}")
+'''
+markets = upbit.load_markets('KRW')
+closed_orders = upbit.fetch_closed_orders('SHIB/KRW')
+balance = upbit.fetch_balance()
+
+
+print(closed_orders)
+'''
